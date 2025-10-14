6bf280cde38b766d3ce0fc1f59aedf603c53ba7d
valynvd <123972183+valynvd@users.noreply.github.com>
2025-10-14 11:45:31 +0700

    Update newstag.js

--- newstag.js
+++ newstag.js
@@ -1,88 +1,63 @@
 (function (root, factory) {
-    if (typeof define == "function" && define.amd) define([], factory);
-    else if (typeof module === "object" && module.exports)
-        module.exports = factory();
-    else root.Newstag = factory();
+  if (typeof define == "function" && define.amd) define([], factory);
+  else if (typeof module === "object" && module.exports)
+      module.exports = factory();
+  else root.Newstag = factory();
 })(this, function () {
 
-    var site, platform, pageType;
+  function init(format, config) {
+    var kly = config.kly || parent.kly || {};
+    var site = (kly.site || "").toLowerCase();
+    var platform = (kly.platform || "").toLowerCase();
 
-    function initNewstag(target, textTag, landingPage, position, klyObj) {
-        site = klyObj.site.toLowerCase();
-        platform = klyObj.platform.toLowerCase();
-        pageType = klyObj.pageType.toLowerCase();
-
-        console.log(target, site, platform);
-
-        switch (site) {
-            case "kapanlagi":
-                return newstagKapanlagi(target, textTag, landingPage, position);
-                break;
+    switch (format) {
+      case "newstag":
+        if (site === "kapanlagi") {
+          NewstagAd(config);
         }
+        break;
     }
+  }
 
-    function newstagKapanlagi(target, textTag, landingPage, position) {
-        if (platform == "desktop") {
-            var tag = target.childNodes[position]; 
+  function NewstagAd(config) {
+    var textTag = config.textTag || "Newstag";
+    var landingPage = config.landingPage || "#";
+    var position = Number.isInteger(config.position) ? config.position : 0;
+    var targetSelector = config.targetSelector || ".header25-trending__list";
+    var count = 0;
 
-            tag = tag.cloneNode(true);
-            tag.querySelector(".header25-trending__item__title").textContent = textTag;
-            tag.setAttribute("href", landingPage);
-            tag.setAttribute("target", "_blank");
-            tag.classList.add("tag-ads");
+    const interval = setInterval(function () {
+      try {
+        var doc = parent.document;
+        if (!doc) return;
 
-            return tag;
-        } else {
-            var tag = target.childNodes[position];
-            tag = tag.cloneNode(true);
-            tag.classList.add("tag-ads");
-            tag.querySelector(".header25-trending__item__title").textContent = textTag;
-            tag.setAttribute("href", landingPage);
-            tag.setAttribute("target", "_blank");
-
-            return tag;
+        var target = doc.querySelector(targetSelector);
+        if (!target) {
+          if (++count > 1000) clearInterval(interval);
+          return;
         }
-    }
 
-    function injectCreative(config) {
-        var textTag = config.textTag || "Newstag";
-        var landingPage = config.landingPage || "#";
-        var position = Number.isInteger(config.position) ? config.position : 0;
-        var targetSelector = config.targetSelector || ".header25-trending__list";
-        var count = 0;
+        var tag = target.childNodes[position];
+        if (!tag) return;
 
-        const CHECK_TAG_ELEMENT = setInterval(function () {
-            try {
-                const doc = parent.document;
-                if (!doc) return;
+        tag = tag.cloneNode(true);
+        tag.classList.add("tag-ads");
+        tag.setAttribute("href", landingPage);
+        tag.setAttribute("target", "_blank");
 
-                const target = doc.querySelector(targetSelector);
-                if (!target) {
-                    if (++count > 1000) clearInterval(CHECK_TAG_ELEMENT)
-                    return;
-                }
+        var title = tag.querySelector(".header25-trending__item__title");
+        if (title) title.textContent = textTag;
 
-                if (target.querySelector(".tag-ads")) {
-                    clearInterval(CHECK_TAG_ELEMENT);
-                    return
-                }
+        var inject = target.querySelectorAll("a.header25-trending__item")[position];
+        if (inject) {
+          inject.insertAdjacentElement("beforebegin", tag);
+          clearInterval(interval);
+        }
+      } catch (e) {
+        clearInterval(interval);
+      }
+    }, 100);
+  }
 
-                var tagItem = target.querySelectorAll("a.header25-trending__item")[position];
-                if (tagItem) {
-                    var GAM_TAG = initNewstag(target, textTag, landingPage, position, parent.kly || {});
-                    if (GAM_TAG) {
-                        tagItem.insertAdjacentElement("beforebegin", GAM_TAG);
-                        clearInterval(CHECK_TAG_ELEMENT);
-                    }
-                }
-                if (++count > 1000) clearInterval(CHECK_TAG_ELEMENT);
-
-            } catch (e) {
-                console.warn("Error", e)
-                clearInterval(CHECK_TAG_ELEMENT);
-            }
-        }, 100)
-    }
-    return { initNewstag, injectCreative };
-
+  return { init };
 });
